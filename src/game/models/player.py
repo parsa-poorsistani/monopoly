4f9e08aec15ad4c57867376f6b9c8fa30fd1ce19
Parsa Poorsistani <poorsistani13@gmail.com>
2023-04-15 21:28:39 +0330

    player model defined

--- src/game/models/player.py
+++ src/game/models/player.py
@@ -1,4 +1,5 @@
-from models.cell import Cell
+from models.cell import Cell,City,Company,Airport
+import random
 
 class Player:
     
@@ -11,10 +12,21 @@ class Player:
         
     def move(self,num_spaces):
         self.location+=num_spaces
-        if self.location >=40:
-            self.location -=40
+        if self.location >40:
+            self.location -=39
             self.balance+=200
         #return self.location
+    
+    def net_worth(self) -> int:
+        worth = 0
+        for prop in self.properties:
+            if isinstance(prop,City):
+                worth+=prop.get_rent+prop.price
+            elif isinstance(prop,Company):
+                worth+=prop.price
+            elif isinstance(prop,Airport):
+                worth+=prop.price
+        return worth
     
     def buy_property(self,property:Cell) -> None:
         self.properties.append(property)
@@ -28,6 +40,17 @@ class Player:
         
     def bankrupted(self):
         self.is_bankrupted = True
+        
+    def roll_dice(self) -> int:
+        dice1 = random.randint(1,6)
+        dice2 = random.randint(1,6)
+        print(f"{self.name} rolled {dice1} and {dice2} ({dice1+dice2})")
+        return dice1+dice2
     
     
+    def __str__(self) -> str:
+        return f"{self.name}\n" \
+            f"location: {self.location}\n" \
+                f"balance: {self.balance}\n" \
+                    f"props: {self.properties}"
     
